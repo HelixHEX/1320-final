ea7e3f9cc7c21ccf2a0ede49cdde176aefb3b37e
Elias <e.wambugu192@gmail.com>
2022-03-04 15:58:11 -0800

    added style data for bands

--- components/band.tsx
+++ components/band.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Stack, Text } from "@chakra-ui/react";
+import { Flex, Heading, ListItem, Stack, Text, UnorderedList } from "@chakra-ui/react";
 import React from "react";
 import Like from "./like";
 
@@ -16,30 +16,50 @@ interface BandProps {
 const Band = ({ band }: BandProps) => {
   return (
     <>
-      <Flex p={10} borderColor={'white'} borderWidth={1} flexDir={"column"}>
-        <Heading color='cyan.400'>{band.band_name}</Heading>
-        <Flex mt={5} flexDir={'column'} color={"white"}>
-          <Flex >
+      <Flex p={10} borderColor={"white"} borderWidth={1} flexDir={"column"}>
+        <Heading color="cyan.400">{band.band_name}</Heading>
+        <Flex mt={5} flexDir={"column"} color={"white"}>
+          <Flex>
             <Text fontWeight={600}>Origin: </Text>
-            <Text ml={2} fontWeight={200}>{band.origin}</Text>
+            <Text ml={2} fontWeight={200}>
+              {band.origin}
+            </Text>
           </Flex>
 
           <Flex>
             <Text fontWeight={600}>Fans: </Text>
-            <Text ml={2} fontWeight={200}>{band.fans}</Text>
+            <Text ml={2} fontWeight={200}>
+              {band.fans}
+            </Text>
           </Flex>
 
-          <Flex >
+          <Flex>
             <Text fontWeight={600}>Formed: </Text>
-            <Text ml={2} fontWeight={200}>{band.formed}</Text>
+            <Text ml={2} fontWeight={200}>
+              {band.formed}
+            </Text>
           </Flex>
 
-          <Flex >
+          <Flex>
             <Text fontWeight={600}>Split: </Text>
-            <Text ml={2} fontWeight={200}>{band.split}</Text>
+            <Text ml={2} fontWeight={200}>
+              {band.split}
+            </Text>
+          </Flex>
+
+          <Flex mt={5}>
+            <UnorderedList>
+              {band.style.split(",").map((style: string, index: number) => (
+                <ListItem key={index}>
+                  <Text key={index} fontWeight={200}>
+                    {style}
+                  </Text>
+                </ListItem>
+              ))}
+            </UnorderedList>
           </Flex>
         </Flex>
-        {band.split === '-' ? <Like /> : null}
+        {band.split === "-" ? <Like /> : null}
       </Flex>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
-import { Flex, Grid, GridItem, SimpleGrid } from "@chakra-ui/react";
+import { Flex, Grid, GridItem, Input, SimpleGrid } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import { useState } from "react";
 import Band from "../components/band";
 import Heading from "../components/heading";
 import data from "../utils/metal.json";
 
 const Home: NextPage = () => {
+  const [search, setSearch] = useState('')
   return (
     <>
       <Flex flexDir={"column"} p={10}>
         <Heading />
+        <Input color='white' variant='flushed' mt={5} placeholder="Search" value={search} onChange={e => setSearch(e.target.value)} />
         <SimpleGrid columnGap={5} mt={10} rowGap={20} columns={[1, 2, 3, 3]} >
-          {data.map((band: BandProps, index: number) => (
+          {data.filter(band => band.band_name.includes(search)).map((band: BandProps, index: number) => (
             <Band key={index} band={band} />
           ))}
         </SimpleGrid>
